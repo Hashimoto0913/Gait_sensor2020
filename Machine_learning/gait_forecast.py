b5fb885ed40ceb0c032ded0493261b78ae009bc4
1701593 <b1701593@planet.kanazawa-it.ac.jp>
2020-11-09 15:20:57 +0900

    Add:status
    結果にクラスタ番号以外も出力するようにした
    クラスタごとの状態の説明を追加した

--- Machine_learning/gait_forecast.py
+++ Machine_learning/gait_forecast.py
@@ -6,34 +6,41 @@ from sklearn.metrics import accuracy_score
 import pickle
 import csv
 
-header = "cluster_id,"
+#対象のデータ
+testdata = 'C:\\Users\\user\\Documents\\Gaitsensor-main\\Machine_learning\\walkingdata.csv'
+
+header = "cluster_id"
 headers = header.split(',')
 
 # pandasのDataFrameを作成
-df = pd.read_csv('D:\ドキュメント\KIT\専門ゼミ\python_code\walkingdata.csv')
+df = pd.read_csv(testdata)
 data = pd.DataFrame(df, columns=['bothfoot_L','swing_L','bothfoot_R','swing_R','stand_L','stand_R'])
 
 #学習済みモデルを読み込み、予測を行う(長期予測)
-bst = pickle.load(open('classifier.pickle', 'rb'))
+bst = pickle.load(open('C:\\Users\\user\\Documents\\Gaitsensor-main\\Machine_learning\\classifier.pickle', 'rb'))
 
 dtest = xgb.DMatrix(data)
 pred = ypred = bst.predict(dtest, ntree_limit=bst.best_ntree_limit)
 l_pred = pred.tolist()
+#クラスタ番号を追加したものを書き出し
+df[header] = pred
+#クラスタ番号から歩行の状態を書き出し
+df['result'] = pred
+df.result = df.result.replace(0, "Run")
+df.result = df.result.replace(1, "Tired")
+df.result = df.result.replace(2, "Normal")
+df.result = df.result.replace(3, "Stop(R)")
+df.result = df.result.replace(4, "Stop(L)")
 
-#初期化
-with open('D:\\ドキュメント\\KIT\\専門ゼミ\\python_code\\prediction_result.csv', 'w',newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(headers)
+#結果を出力
+df.to_csv('C:\\Users\\user\\Documents\\Gaitsensor-main\\Machine_learning\\prediction_result.csv')
 
-with open('D:\\ドキュメント\\KIT\\専門ゼミ\\python_code\\prediction_result.csv', 'a',newline="") as f:
-    writer = csv.writer(f)
-    for ele in l_pred:
-        writer.writerow([ele])
+'''
+#精度の計算 Train.csvの元のデータからprediction_result.csvを作成した場合のみ可能
+#新規のデータは正解ラベルが存在しないため精度がわからない
 
-#精度の計算
-df_train = pd.read_csv('D:\ドキュメント\KIT\専門ゼミ\python_code\Train.csv')
-df_result = pd.read_csv('D:\ドキュメント\KIT\専門ゼミ\python_code\prediction_result.csv')
-
+df_train = pd.read_csv('C:\\Users\\user\\Documents\\Gaitsensor-main\\Machine_learning\\Train.csv')
+df_result = pd.read_csv('C:\\Users\\user\\Documents\\Gaitsensor-main\\Machine_learning\\prediction_result.csv')
 origin = pd.DataFrame(df_train, columns=['cluster_id'])
 result = pd.DataFrame(df_result, columns=['cluster_id'])
 
@@ -41,9 +48,10 @@ result = pd.DataFrame(df_result, columns=['cluster_id'])
 n = len(origin)
 print("Number of data:",n)
 #誤認識数
-k = len(origin[~origin.isin(result.to_dict(orient='list')).all(1)])
-print("Incorrect answer:",k)
-print(origin[~origin.isin(result.to_dict(orient='list')).all(1)],"\n")
+k = origin[~origin.isin(result.to_dict(orient='list')).all(1)]
+print("Incorrect answer:",len(k))
+if len(k) != 0: print(k,"\n")
 
 #精度
-print("accuracy:" , round(1 - k/n,4))
+print("accuracy:" , round(1 - len(k)/n,4))
+'''
